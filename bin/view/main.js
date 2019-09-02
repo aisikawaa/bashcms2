23fcb0981199cecf11a083d5b4c55339fdd082cb
aisikawaa <alterthemadness@outlook.com>
2019-09-02 12:29:56 +0900

    link script

--- bin/view/main.js
+++ bin/view/main.js
@@ -1,5 +1,6 @@
 window.onload = function (){
     lastArticles(10);
+    linkKeywords();
 }
 
 function lastArticles(num){
@@ -11,6 +12,18 @@ function lastArticles(num){
         document.getElementById("last-articles").innerHTML = httpReq.responseText;
     }
     var url = "/last_articles.cgi?num=" + num;
+    httpReq.open("GET",url,true);
+    httpReq.send(null);
+}
+
+function linkKeywords(){
+    var httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function(){
+        if(httpReq.readyState != 4 || httpReq.status != 200)
+            return;
+    }
+    var word = document.getElementById("Keywords").innerHTML = httpReq.responseText;
+    var url = "/link_keywords.cgi?keywords=" + encodeURIComponent(word);
     httpReq.open("GET",url,true);
     httpReq.send(null);
 }
