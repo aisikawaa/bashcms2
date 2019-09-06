2ddd9c78c84a59f1b6862ba5b1fa2b969f0cf398
aisikawaa <alterthemadness@outlook.com>
2019-09-06 20:08:19 +0900

    typo fix

--- bin/view/main.js
+++ bin/view/main.js
@@ -31,7 +31,7 @@ function linkKeywords(){
     httpReq.send(null);
 }
 
-function fullSeach(){
+function fullSearch(){
     var httpReq = new XMLHttpRequest();
     httpReq.onreadystatechange = function(){
         if(httpReq.readyState != 4 || httpReq.status != 200)
